5e5acf2db84e889590a4103ff58af4ee7c979087
amitsengupta <amitsengupta332@gmail.com>
2025-09-20 22:54:29 +0600

    blog card and latest blog

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ const NotFoundPage = () => {
   return (
     <div className="w-[90%] mx-auto">
       <Image
-        src="https://freefrontend.com/assets/img/html-css-404-page-templates/Pure-CSS-404-Error-Page.gif"
+        src="https://img.freepik.com/free-vector/hand-drawn-404-error_23-2147738999.jpg?ga=GA1.1.630025796.1757690952&semt=ais_incoming&w=740&q=80"
         width={1000}
         height={500}
         alt="not found page"
