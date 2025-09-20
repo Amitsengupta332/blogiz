2a8d17b82101ab856e94a0bafa38d76279b07cf8
amitsengupta <amitsengupta332@gmail.com>
2025-09-21 01:47:47 +0600

    ssg vid05

--- src/app/blogs/[blogId]/page.tsx
+++ src/app/blogs/[blogId]/page.tsx
@@ -1,22 +1,29 @@
-
 // interface BlogId {
 //     params: {
 //         blogId: string;
 //     }
 
 import BlogDetails from "@/components/ui/BlogDetails";
-import { BlogId } from "@/types";
+import { Blog, BlogId } from "@/types";
 
-// }
+const generateStaticParams = async () => {
+  const res = await fetch("http://localhost:5000/blogs");
+  const blogs = await res.json();
+
+  return blogs.slice(0, 3).map((blog: Blog) => ({
+    blogId: blog.id,
+  }));
+};
+
 const BlogDetailPage = async ({ params }: BlogId) => {
-    const res = await fetch(`http://localhost:5000/blogs/${params.blogId}`,{
-        cache: "no-store",
-    });
-    const blog = await res.json();
-    // console.log(blog);
+  const res = await fetch(`http://localhost:5000/blogs/${params.blogId}`, {
+    cache: "no-store",
+  });
+  const blog = await res.json();
+  // console.log(blog);
   return (
     <div className="my-10">
-       <BlogDetails blog={blog} />
+      <BlogDetails blog={blog} />
     </div>
   );
 };
